7f2b3ec4301979e6115353e50afd9dd35f74c8ce
jenniferwiner <jeniwiner@gmail.com>
2017-08-23 10:29:49 -0600

    plan approach

--- recoverSecretString/recoverSecret.js
+++ recoverSecretString/recoverSecret.js
@@ -1,5 +1,14 @@
 const recoverSecret = (triplets) => {
-//
+  // start at first array
+  let array = [...triplets[0]]
+  // make copy without triplets[0], will be decreceasin
+  let copy = triplets.slice(1)
+  while (copy.length > 0) {
+    for (let i = 1; i < triplets.length; i++) {
+      // check triplet[i][...] exists in array
+      // order is important
+    }
+  }
 }
 
 module.exports = recoverSecret
